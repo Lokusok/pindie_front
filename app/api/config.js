d3e1cf1a8530eca87e9cb3e98c82f15631046d3a
Lokusok <lokusokdrobk@gmail.com>
2024-05-18 14:07:33 +0300

    update base_url

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://backlokusok.nomoredomainswork.ru/api';
+export const BASE_URL = 'https://backlokusok.nomoredomainswork.ru/api';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
